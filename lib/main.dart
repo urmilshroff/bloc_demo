27cbef44b70f40fcd2bb60bc0c89299f9dd770a0
urmilshroff <urmilshroff@gmail.com>
2020-07-04 17:41:41 +0530

    Revert "Poor implementation of persiting count value"
    This reverts commit 759f3bdd8a97197e13bfd30c09c14df2087e8e51.

--- lib/main.dart
+++ lib/main.dart
@@ -2,27 +2,14 @@ import 'package:bloc_demo/blocs/counter_bloc.dart';
 import 'package:bloc_demo/pages/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  SharedPreferences prefs = await SharedPreferences.getInstance();
-  runApp(MyApp(
-    prefs: prefs,
-  ));
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  SharedPreferences prefs;
-  MyApp({
-    @required this.prefs,
-  });
   @override
   Widget build(BuildContext context) {
-    final count = prefs.getInt('count') ?? 0;
-
     return BlocProvider(
-      create: (BuildContext context) => CounterBloc(count), // this is the bloc!
+      create: (BuildContext context) => CounterBloc(0), // this is the bloc!
       // allows MaterialApp to access CounterBloc
       child: MaterialApp(
         title: 'BLoC Demo',
